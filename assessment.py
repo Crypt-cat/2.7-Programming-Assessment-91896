d06f47ac0e7b0b553875d928049adccc25fddb5f
Crypt-cat <tomibrew806@gmail.com>
2025-04-08 17:45:03 +1200

    Third Commit
    Practically finished all the functions. Touched up some stylistic stuff. Still have a bit more tidying up to do.

--- assessment.py
+++ assessment.py
@@ -4,7 +4,11 @@ os.system("clear")
 
 user_money = 0
 star_rating = 0
-
+MAX_RATING = 5
+MONEY_BOOST = 1.5
+BREWSTER_BURGER = 25
+PROFIT_MULTIPLIER = 100
+money_upgrade = False
 
 class Burger:
     """Represents a burger."""
@@ -64,7 +68,7 @@ def get_ingredient(question):
 
 def menu():
     while True:
-        user_input = get_int("Welcome to Brewster's Burgers!\n\n1. Take an order\n2. Show ingredients list\n3. Upgrades\n4. Evaluate star rating\n5. Exit\n\n> ")
+        user_input = get_int("Welcome to Brewster's Burgers!\n\n1. Take an order\n2. Show ingredients list\n3. Upgrades and money\n4. Show star rating\n5. Exit\n\n> ")
         if not user_input in [1, 2, 3, 4, 5]:
             print("Invalid option.")
             time.sleep(2)
@@ -79,19 +83,85 @@ def menu():
     elif user_input == 3:
         upgrades()
     elif user_input == 4:
-        star_rating()
+        show_rating()
     elif user_input == 5:
         exit()
 
 
 def ingredients_list():
+    """This function shows the user the ingredients needed for specific burgers. It lays it out in a way that is easy to understand."""
     print(f"Memorise the burger's ingredients to earn money from your orders!\n\n| {'Burger Type:':16s}| Ingredients:\n|-----------------|-----------------------------------------------------------------------------------")
     for burger in current_burgers:
         print(f"| {burger.burger_type:16s}| {burger.ingredients}\n|-----------------|-----------------------------------------------------------------------------------")
     print("\n")
 
 
+def show_rating():
+    """This function allows the user to see their star rating. It will tell them a message depending on what it is and this is useful to check for the requirement for the Brewster Burger."""
+    reveal_text("Searching through reviews...")
+    time.sleep(2)
+    os.system("clear")
+    print(f"Your restaurant has a star rating of {star_rating} out of 5 on Google.\nTake customer orders correctly to improve your rating (unless it's at 5).\nRemember that getting an order wrong will hurt your rating!")
+    if star_rating < 3:
+        print("\nYou might have to step up your game...")
+    elif star_rating == 3:
+        print("\nYou're doing okay.")
+    else:
+        print("\nYou're doing a great job at running this place!")
+    print("\n")
+
+
+def upgrades():
+    """This function allows the user to access upgrades where the user can unlock the new Brewster Burger or multiply their overall profit. It tell them how much money they need and if they already have it
+    The Brewster Burger also has a star rating requirement, which depends on how well the user takes orders. It also shows you how much money you have and a message depending on that"""
+    global user_money
+    global money_upgrade
+    reveal_text("Counting up money...")
+    time.sleep(2)
+    os.system("clear")
+    if user_money <= 30:
+        print("\nYou might need to go make some more money...")
+    elif user_money < 100:
+        print("\nThat's not bad.")
+    else:
+        print("\nYou're rich!")
+    while True:
+        user_input = get_int(f"You currently have {user_money} dollars.\n\n1. Unlock Brewster Burger (${BREWSTER_BURGER}) \n2. Overall 1.5x profit multiplier (${PROFIT_MULTIPLIER})\n3. Go Back\n\n> ")
+        if not user_input in [1, 2, 3]:
+            print("Invalid option.")
+            time.sleep(2)
+            os.system("clear")
+            continue
+        else:
+            break
+    if user_input == 1:
+        if len(current_burgers) == 7:
+            print("You've already bought this.")
+        elif star_rating < MAX_RATING:
+            print(f"Sorry, you need a star rating of {MAX_RATING} out of 5 to unlock this.")
+        elif user_money < BREWSTER_BURGER:
+            print(f"You don't have enough money for this, you need {BREWSTER_BURGER - user_money} more dollars.")
+        else:
+            print("Successfully bought.")
+            user_money = user_money - 25
+            current_burgers.append(brewster_burger)
+    elif user_input == 2:
+        if money_upgrade == True:
+            print("You've already bought this.")
+        elif user_money < PROFIT_MULTIPLIER:
+            print(f"You don't have enough money for this, you need {PROFIT_MULTIPLIER - user_money} more dollars.")
+        else:
+            print("Successfully bought.")
+            user_money = user_money - 100
+            money_upgrade = True
+    else:
+        menu()
+    print("\n")
+
+
 def take_order():
+    """This function allows the user to take orders from customers and make money. This money allows them to buy upgrades. You also gain star rating depending on how you do.
+    The program will tell you if you got their order right or not."""
     global user_money
     global star_rating
     customer_order = random.choice(current_burgers)
@@ -109,8 +179,11 @@ def take_order():
         reveal_text("Thank you for getting my order correct!")
         time.sleep(3)
         os.system("clear")
-        user_money = user_money + customer_order.price
-        if star_rating < 5:
+        if money_upgrade == True:
+            user_money = user_money + (customer_order.price*MONEY_BOOST)
+        else:
+            user_money = user_money + customer_order.price
+        if star_rating < MAX_RATING:
             star_rating = star_rating + 1
     else:
         reveal_text("This isn't what I ordered!")
@@ -119,8 +192,6 @@ def take_order():
         if star_rating > 0:
             star_rating = star_rating - 1
 
+
 while True:
-    print(star_rating)
-    print(user_money)
     menu()
-    
